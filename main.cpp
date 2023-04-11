71cb02bc5ef1b27c9ae6897cc3954e5cdd100e29
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-04-11 15:30:41 -0500

    starting threads

--- main.cpp
+++ main.cpp
@@ -4,21 +4,43 @@ using namespace std;
 #include <pthread.h>
 #include <vector>
 #include <unistd.h>
+#include <sstream>
 
-struct ship{
-    string BoatName;
-    int arrivalTime;// the time a ship arrives at the bridge
-    int TimeForShipToCross;// the tim it takes for the car to cross the bridge
+struct bridge{
+    int liftingBridge;// the time elapsed since the arrival of the prevous car or ship
+    int loweringBridge;// the tim it takes for the car to cross the bridge
 };
-
-struct car{
+struct vehicle{
+    string vehicletype;
     string licensePlateNumber;
     int arrivalTime;// the time elapsed since the arrival of the prevous car or ship
     int TimeForCarToCross;// the tim it takes for the car to cross the bridge
 };
 
+void* threadFunction(void* arg) {
+    // cast the argument to a pointer to the ThreadData struct
+    vehicle* threadData = static_cast<vehicle*>(arg);
 
+    // print out the values in the struct
+    cout << "vehicle type: " << threadData-> vehicletype << endl;
+    cout << "license Plate Number: " << threadData->licensePlateNumber << endl;
+    cout << "arrival Time: " << threadData-> arrivalTime << endl;
+    cout << "Time For Cross: " << threadData->TimeForCarToCross << endl;
+    cout<<"=====================================";
+    return nullptr;
+}
 int main() {
-    cout << "Hello, World!" << endl;
+    int numThreads=4;
+    struct vehicle transportation[numThreads];
+    string FileReadin;
+    //cout << "Hello, World!" << endl;
+    while(getline(cin,FileReadin)){
+        cout<<FileReadin<<endl;
+        stringstream  ss(FileReadin);
+        string word;
+        while(ss>>word){
+
+        }
+    }
     return 0;
 }
