1ce9803180f2bce83448a78a5de823dfa8aa1eac
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-04-23 12:21:14 -0500

    Getting the correct output(working & completed)

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #include <fstream>
 
 enum drawbridgeStatus {NOCARS, CARSCANGO};
-drawbridgeStatus DrawbridgeStatus=NOCARS;
+drawbridgeStatus DrawbridgeStatus=CARSCANGO;
 pthread_cond_t carsCanGoCond=PTHREAD_COND_INITIALIZER;
 int bridgeTimeToRaiseDrawbridge=1;
 int bridgeTimeToLowerDrawbridge=1;
@@ -25,35 +25,10 @@ struct vehicle{
     int TimeForCarToCross;// the tim it takes for the car to cross the bridge
 };
 
-
 void* shipRoutine(void* arg){
-    /***********STEPS FOR SHIP ROUTINE***********************
-   * STEP 1:Close the drawbridge for car trafic
-   * STEP 2: Wait until they can lower the drawbridge(No other ship & No car on the drawbridge)
-   * STEP 3:Sleep for drawbridge raising time
-   * STEP 4: Sleep for bridge crossing time
-   * STEP 5: Sleep for drawbridge lowering time
-   * STEP 6: Reopen drawbridge to car traffic
-   * ============IMPLEMENTATION STEPS==============================
-   * implement these instructions:
-   * STEP 1: Print a message when they are created: "Ship [Name] arrives"
-   * STEP 2: Set drawbridgeStatus to NOCARS(Before doing anything else)
-   * STEP 3: Print second message: "Drawbridge is closed to car traffic"
-   * STEP 4: Request drawbridge mutex( No cars on bridge)
-   * STEP 5:Update nShips
-   * STEP 6: Print a third messsage: "Drawbidge can be saftely raised
-   * STEP 7: Print a fourth message: "Ship [Name] goes"
-   * STEP 8: Sleep for drawbridge crossing time
-   * STEP 9: Print a fifth message: "Ship [Name] leaves"
-   * STEP 10: Sleep for draw bridge lowering time
-   * STEP 11: print a sixth message: "Drawbridge is reopended to car traffic"
-   * STEP 12: Set drawbridgeStatus to CARSCANGO
-   * STEP 13: Signal CARSCANGO condition
-   * STEP 14: Release drawbridge mutex (Reopens drawbridge to care traffic)
-   * ****************************************************/
     vehicle threadData = *(vehicle*)arg;
     string shipName=threadData.licensePlateNumber;
-    cout<<"Ship "<<shipName<< " arrives"<<endl;
+    cout<<"Ship "<<shipName<< " arrives at the drawbridge"<<endl;
     DrawbridgeStatus=NOCARS;
     cout<<"Drawbridge is closed to car traffic"<<endl;
     pthread_mutex_lock(&drawBridgeLock);
@@ -78,24 +53,14 @@ void* shipRoutine(void* arg){
     return nullptr;
 }
 void* carRoutine(void* arg){
-    /**************STEPS FOR CAR ROUTINE********************************
-     * STEP 1: print message: car i has arrived
-     * STEP 2: Request drawbridge mutex
-     * STEP 3: Update nCars(From inside a critical section)
-     * STEP 4: if drawbridgeStatus == NoCARS( Wait for a carCanGo signal from a ship thread)
-     * STEP 5: Print second message: " Car i Goes on Bridge"
-     * STEP 6: Sleep for the time it takes to cross the drawbridge
-     * STEP 7: Print a third message: "Car i Leaves the drawbridge"
-     * STEP 8: drawbridge mutex
- * ********************************************************/
     vehicle threadData = *(vehicle*)arg;
     string carName=threadData.licensePlateNumber;
-    cout<<"Car "<<carName<< " has Arrived"<<endl;
+    cout<<"Car "<<carName<< " arrives at the drawbridge"<<endl;
     pthread_mutex_lock(&drawBridgeLock);
 
     nCars++;
 
-    while(DrawbridgeStatus==NOCARS){
+    if(DrawbridgeStatus==NOCARS){
         pthread_cond_wait(&carsCanGoCond,&drawBridgeLock);
     }
 cout<<"Car "<<carName<<" goes on the drawbridge"<<endl;
@@ -135,50 +100,43 @@ int main() {
         cerr << "Failed to open file" << std::endl;
         return 1;
     }
-    while(getline(file,FileReadin)){
+    while (getline(file, FileReadin)) {
         //cout<<FileReadin<<endl;
-        stringstream  ss(FileReadin);
+        stringstream ss(FileReadin);
         string type, name;
-        int x,y;
-        ss>>type>>name>>x>>y;
-        if(type== "Bridge"){
-            temp1=x;
-            temp2=y;
-        }else if(type=="Car"||type=="Ship"){
+        int x, y;
+        ss >> type >> name >> x >> y;
+        if (type == "Bridge") {
+            temp1 = x;
+            temp2 = y;
+        } else if (type == "Car" || type == "Ship") {
             vehicle Vehicle;
-            Vehicle.vehicletype=type;
-            Vehicle.licensePlateNumber=name;
-            Vehicle.arrivalTime=x;
-            Vehicle.TimeForCarToCross=y;
+            Vehicle.vehicletype = type;
+            Vehicle.licensePlateNumber = name;
+            Vehicle.arrivalTime = x;
+            Vehicle.TimeForCarToCross = y;
             transportation.push_back(Vehicle);
         }
     }
-    pthread_t pid[transportation.size()];
-    for(int i=0; i<transportation.size();i++){
-        vehicle* temp= new vehicle(transportation[i]);
-        sleep(temp->arrivalTime);
-        if(pthread_create(&pid[i],NULL,&threadFunction,temp) !=0){
-            perror("Failed to create thread");
-            return 1;
-        }
-    }
-    for(int i=0; i<transportation.size();i++){
-        if(pthread_join(pid[i],NULL)!=0){
-            return 2;
-        }
-        //cout<<" Thread "<<i<<" has ended execution"<<endl;
-    }
-    cout<<nCars<<" Cars(s) crossed the bridge."<<endl;
-    cout<<nShips<<" Ship(s) crossed the bridge."<<endl;
-//    cout<<"==============Bridge======================"<<endl;
-//    cout << "Bridge Time To Raise Drawbridge: " << bridgeTimeToRaiseDrawbridge<< endl;
-//    cout << "Bridge Time To Lower Drawbridge: " << bridgeTimeToLowerDrawbridge << endl;
-//    cout<<"====================================="<<endl;
-//    for(int i= 0; i< transportation.size();i++){
-//        cout << "vehicle type: " << transportation[i].vehicletype << endl;
-//        cout << "license Plate Number: " << transportation[i].licensePlateNumber << endl;
-//        cout << "arrival Time: " << transportation[i].arrivalTime << endl;
-//        cout << "Time For Cross: " << transportation[i].TimeForCarToCross << endl;
-//        cout<<"====================================="<<endl;
+    cout<<"Bridge Rise Time: "<<temp1<<endl;
+    cout<<"Bridge lower Time: "<<temp2<<endl;
+//    pthread_t pid[transportation.size()];
+//    for (int i = 0; i < transportation.size(); i++) {
+//        vehicle *temp = new vehicle(transportation[i]);
+//        sleep(temp->arrivalTime);
+//        if (pthread_create(&pid[i], NULL, &threadFunction, temp) != 0) {
+//            perror("Failed to create thread");
+//            return 1;
+//        }
+//    }
+//    for (int i = 0; i < transportation.size(); i++) {
+//        if (pthread_join(pid[i], NULL) != 0) {
+//            return 2;
+//        }
+//    }
+//    cout << nCars << " Cars(s) crossed the bridge." << endl;
+//    cout << nShips << " Ship(s) crossed the bridge." << endl;
     return 0;
 }
+
+
